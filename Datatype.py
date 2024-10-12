8a051d51812213d0e9931abaab028cdc731b4648
Daksh-coder-oss <vv0039190@gmail.com>
2024-10-12 10:19:36 +0100

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,18 @@
+A= 45
+B= 7.5
+C= "hello"
+D= True
+print("A datatype is :",type (A))
+
+
+
+
+
+print("B datatype is :",type (B))
+
+print("C datatype is :",type (C))
+
+print("D datatype is :",type (D))
+
+
+
---
+++ Typecasting.py
@@ -0,0 +1,5 @@
+name=input("Enter your name")
+print("Name entered is:",name,"its datatype is:",type(name))
+
+age=input("Enter your age")
+print("Age entered is:",age,"its datatype is:",type(int(age)))
